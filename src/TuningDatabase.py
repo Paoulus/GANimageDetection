34ae6286b7b1c7bd35924b5159619487321e9bbf
paolo <paolo.chiste-2@studenti.unitn.it>
2022-06-04 11:40:13 +0200

    Added folders of DatasetFast to dataset

--- src/TuningDatabase.py
+++ src/TuningDatabase.py
@@ -11,13 +11,13 @@ class TuningDatabase(datasets.DatasetFolder):
         self.transform = transform
         for entry in os.listdir(path):
             data_folder = os.path.join(path, entry)
-            if entry == 'FFHQ':
+            if entry == 'FFHQ' or entry == "Real":
                 for root, dirs, files in os.walk(data_folder):
                     for file in files:
                         if file.endswith(".png"):
                             item = os.path.join(root, file), 0
                             self.samples.append(item)
-            if entry == 'styleGAN' or entry == 'StyleGAN2':
+            if entry == 'styleGAN' or entry == 'StyleGAN2' or entry =="Fake":
                 for root, dirs, files in os.walk(data_folder, topdown=True):
                     dirs[:] = [d for d in dirs if d not in exclude]
                     for file in files:
