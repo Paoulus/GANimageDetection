390ea03d14520a60a75d71d8053bfe671cd5deaf
paolo <paolo.chiste-2@studenti.unitn.it>
2022-09-23 14:43:03 +0200

    fixed some imports.

--- src/main.py
+++ src/main.py
@@ -11,138 +11,29 @@
 # http://www.grip.unina.it/download/LICENSE_OPEN.txt
 #
 from datetime import datetime
+from time import time
 import os
-import glob
 import argparse
 import json
+import pandas as pd
+import numpy as np
 from PIL import Image
 from resnet50nodown import resnet50nodown
-from torch import utils,arange, tensor
+from torch import torch
 from torch import save as save_model
+import torch.nn as nn
+import tqdm
+import torch.optim as optim
 from torch.utils.data import DataLoader, Subset, random_split
 from torch.cuda import is_available as is_available_cuda
-from torch.cuda import empty_cache
 from torchvision import transforms
 from Databases import TuningDatabaseFromFile, TuningDatabaseWithRandomSampling, TuningDatabaseFromSamples
-import csv
-import random
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description="This script fine-tunes the original resnet50 model from GANimageDetection.",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--config_path', '-c', type=str, default='config.json')
-    parser.add_argument('--weights_path', '-m', type=str, default='./weights/gandetection_resnet50nodown_stylegan2.pth',
-                        help='weights path of the network')
-    parser.add_argument('--device_to_use', '-d', type=str, default='cuda:0',
-                        help='device to use for fine tuning (values: cpu, cuda:0)')
-    script_arguments = parser.parse_args()
-    
-    config_file = open(script_arguments.config_path,"r")
-    settings_json = json.loads(config_file.read())
-
-    weights_path = script_arguments.weights_path
-    device_to_use = script_arguments.device_to_use
-    input_folder = settings_json["DatasetPath"]
-    resume_from_checkpoint = settings_json["LoadCheckpoint"]
-    batch_size = settings_json["BatchSize"]
-    
-    # generate folder names and paths for the current execution report folder
-    today = datetime.now()
-    date_folder = today.strftime("%m_%d_%H_%M_%S")
-    logs_folder = os.path.join("logs",date_folder)
-    finetuned_weights_path = os.path.join(logs_folder,"finetuned_weights.pth")
-    checkpoints_path = os.path.join(logs_folder,"checkpoints")
-
-    if not os.path.exists(logs_folder):
-        os.mkdir(logs_folder)
-
-    # load dataset and setup dataloaders and transforms (dataloader is used for batching)
-    transforms = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-
-    total_dataset = TuningDatabaseWithRandomSampling(input_folder,transforms,seed=13776321)
-
-    for path,_ in total_dataset.samples:
-        print(path)
-    
-    test_dataset = TuningDatabaseFromFile("test-set-minuscule.txt")
-    for el in test_dataset.samples:
-        if el in total_dataset.samples:
-            total_dataset.samples.remove(el)
-
-    test_set_size = len(test_dataset.samples)
-    validation_set_size = int(test_set_size * 0.2 )
-    train_set_size = len(total_dataset.samples)-validation_set_size
-    
-    train_dataset,validation_dataset = random_split(total_dataset,[train_set_size,validation_set_size])
-
-    databases = {
-        'train': train_dataset,
-        'testing': test_dataset,
-        'val': validation_dataset
-    }
-
-    dataloaders = {
-        x:DataLoader(databases[x],batch_size=batch_size,shuffle=True) for x in ['train','testing','val']
-    }
-
-    # retrieve the data directly from the database, so we do not call the transform on the
-    # sample just to read the label
-    real_images_count = 0
-    fake_images_count = 0
-    
-    train_dataset_files = []
-    for index in databases['train'].indices:
-        if total_dataset.samples[index][1] == 0 : 
-            real_images_count += 1
-        else:
-            fake_images_count += 1
-        train_dataset_files.append(total_dataset.samples[index][0])
-
-    print("TRAINING INFO")
-    print(f"Using device {device_to_use}")
-    print("Dataset located at: {}".format(input_folder))
-    print("Will train with {} images and test with {} images".format(len(databases['train']),len(databases['testing'])))
-    print("Training dataset composition: \n {} samples labeled real \n {} samples labeled fake".format(real_images_count,fake_images_count))
-    print(10*"=")
-    print("Training on images")
-    for filename in train_dataset_files:
-        print(filename)
-    print(10*"=")
-    print("Validating on")
-    for index in databases['val'].indices:
-        print(total_dataset.samples[index][0])
-    print("Testing on")
-    for path,label in databases["testing"].samples:
-        print(path)
-
-    configuration = {
-                "device_to_use":device_to_use,
-                "checkpoints_path":checkpoints_path,
-                "num_classes":num_classes,
-                "device":device_to_use,
-                "perform_validation":perform_validation,
-                "perform_testing":perform_testing
-                }
-
-    resnet_no_down_model = resnet50nodown(device_to_use, weights_path)
-    print("Training started on {}".format(datetime.now().strftime("%b %a %d at %H:%M:%S")))
-    fine_tuned_model, accuracy_history = fineTune(resnet_no_down_model,dataloaders,configuration)
-    print("Training ended on {}".format(datetime.now().strftime("%b %a %d at %H:%M:%S")))
-
-    testModel(fine_tuned_model,dataloaders,configuration['device_to_use'])
-
-    print("Checkpoints are located at {}".format(checkpoints_path))
-    print("Saving fine-tuned model (most recent checkpoint) in {}".format(finetuned_weights_path))
-    save_model(fine_tuned_model.state_dict(), finetuned_weights_path)
+from sklearn.metrics import confusion_matrix
 
 # return a fine-tuned version of the original resnet50 model
 # by default num_classes is = 1, since it's specified like that in the original code
 def fineTune(model, database, configuration):
-    if num_classes < 2 :
+    if configuration["num_classes"] < 2 :
         loss_fn = nn.BCEWithLogitsLoss()
     else:
         loss_fn = nn.CrossEntropyLoss()
@@ -166,7 +57,7 @@ def fineTune(model, database, configuration):
     for name,param in model.named_parameters():
         if param.requires_grad == True:
             print("\t",name)
-    required_epochs = epochs - checkpoint_epoch
+    required_epochs = configuration["epochs"] - checkpoint_epoch
 
     training_start = time.time()
     validation_history = train_loop(model, database, loss_fn, optimizer, configuration['device'], 
@@ -351,3 +242,114 @@ def testModel(model,dataloaders,device):
 
     with open("Acc_Loss.txt", "a") as acclss:
         acclss.write('Acc:{}\n'.format(accuracy))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="This script fine-tunes the original resnet50 model from GANimageDetection.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--config_path', '-c', type=str, default='config.json')
+    parser.add_argument('--weights_path', '-m', type=str, default='./weights/gandetection_resnet50nodown_stylegan2.pth',
+                        help='weights path of the network')
+    parser.add_argument('--device_to_use', '-d', type=str, default='cuda:0',
+                        help='device to use for fine tuning (values: cpu, cuda:0)')
+    script_arguments = parser.parse_args()
+    
+    config_file_fs = open(script_arguments.config_path,"r")
+    settings_json = json.loads(config_file_fs.read())
+
+    weights_path = script_arguments.weights_path
+    device_to_use = script_arguments.device_to_use
+    input_folder = settings_json["DatasetPath"]
+    resume_from_checkpoint = settings_json["LoadCheckpoint"]
+    batch_size = settings_json["BatchSize"]
+    
+    # generate folder names and paths for the current execution report folder
+    today = datetime.now()
+    date_folder = today.strftime("%m_%d_%H_%M_%S")
+    logs_folder = os.path.join("logs",date_folder)
+    finetuned_weights_path = os.path.join(logs_folder,"finetuned_weights.pth")
+    checkpoints_path = os.path.join(logs_folder,"checkpoints")
+
+    if not os.path.exists(logs_folder):
+        os.mkdir(logs_folder)
+
+    # load dataset and setup dataloaders and transforms (dataloader is used for batching)
+    transform_convert_and_normalize = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    total_dataset = TuningDatabaseWithRandomSampling(input_folder,transform_convert_and_normalize,seed=13776321)
+
+    for path,_ in total_dataset.samples:
+        print(path)
+    
+    test_dataset = TuningDatabaseFromFile("test-set-minuscule.txt")
+    for el in test_dataset.samples:
+        if el in total_dataset.samples:
+            total_dataset.samples.remove(el)
+
+    test_set_size = len(test_dataset.samples)
+    validation_set_size = int(test_set_size * 0.2 )
+    train_set_size = len(total_dataset.samples)-validation_set_size
+    
+    train_dataset,validation_dataset = random_split(total_dataset,[train_set_size,validation_set_size])
+
+    databases = {
+        'train': train_dataset,
+        'testing': test_dataset,
+        'val': validation_dataset
+    }
+
+    dataloaders = {
+        x:DataLoader(databases[x],batch_size=batch_size,shuffle=True) for x in ['train','testing','val']
+    }
+
+    # retrieve the data directly from the database, so we do not call the transform on the
+    # sample just to read the label
+    real_images_count = 0
+    fake_images_count = 0
+    
+    train_dataset_files = []
+    for index in databases['train'].indices:
+        if total_dataset.samples[index][1] == 0 : 
+            real_images_count += 1
+        else:
+            fake_images_count += 1
+        train_dataset_files.append(total_dataset.samples[index][0])
+
+    print("TRAINING INFO")
+    print(f"Using device {device_to_use}")
+    print("Dataset located at: {}".format(input_folder))
+    print("Will train with {} images and test with {} images".format(len(databases['train']),len(databases['testing'])))
+    print("Training dataset composition: \n {} samples labeled real \n {} samples labeled fake".format(real_images_count,fake_images_count))
+    print(10*"=")
+    print("Training on images")
+    for filename in train_dataset_files:
+        print(filename)
+    print(10*"=")
+    print("Validating on")
+    for index in databases['val'].indices:
+        print(total_dataset.samples[index][0])
+    print("Testing on")
+    for path,label in databases["testing"].samples:
+        print(path)
+
+    configuration = {
+                "checkpoints_path":checkpoints_path,
+                "num_classes":2,
+                "device":device_to_use,
+                "perform_validation":settings_json["PerformValidation"],
+                "perform_testing":settings_json["PerformTesting"]
+                }
+
+    resnet_no_down_model = resnet50nodown(device_to_use, weights_path)
+    print("Training started on {}".format(datetime.now().strftime("%b %a %d at %H:%M:%S")))
+    fine_tuned_model, accuracy_history = fineTune(resnet_no_down_model,dataloaders,configuration)
+    print("Training ended on {}".format(datetime.now().strftime("%b %a %d at %H:%M:%S")))
+
+    testModel(fine_tuned_model,dataloaders,configuration['device_to_use'])
+
+    print("Checkpoints are located at {}".format(checkpoints_path))
+    print("Saving fine-tuned model (most recent checkpoint) in {}".format(finetuned_weights_path))
+    save_model(fine_tuned_model.state_dict(), finetuned_weights_path)
